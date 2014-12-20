0112fc5f32e7490c0d29df93739528349f571bac
tibaldev <tibaldi.tibalte@mailoo.org>
2014-12-20 14:29:48 +0100

    utilisation du router

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 module.exports = function(app, appData) {
     // GET - Accueil
-    app.get('/', function (req, res) {
+    app.route('/').get(function (req, res) {
         res.render('index', { 'title': appData.title});
     });
 
-    app.get('*', function(req, res) { 
+    // GET - 404
+    app.route('*').get(function(req, res) { 
         res.status(404).render('404', { title: appData.title });
     });
 };
